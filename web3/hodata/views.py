ad5a34c763f7cfbc7e4652fc132df42fd8f61f22
ViktorErema <erema.1996@bk.ru>
2023-11-14 17:46:49 +0300

    На потом сохраняю

--- web3/hodata/views.py
+++ web3/hodata/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 from .forms import DocumentForm
 from .models import Document
@@ -7,6 +7,10 @@ from .models import Document
 def document_list(request):
     documents = Document.objects.all()
     return render (request, 'web3/document_list.html', {'items': documents})
+
+def document_detail(request,document_pk):
+    documents = Document.objects.get(pk=document_pk)
+    return render(request, 'web3/document_detail.html', {'doc': documents})
 
 def document_new(request):
     if request.method == "GET":
@@ -19,3 +23,18 @@ def document_new(request):
             document.save()
             return redirect(document_list)
 
+# def zapros (request, document_pk):
+#     documents = Document.objects.get(pk=document_pk)
+#     return render(request, 'web3/document_detail.html', {
+#                                                         'items': documents,
+#                                                                         })
+
+def zapros(request):
+    if request.method == 'POST':
+        form = DocumentForm(request.POST)
+        if form.is_valid():
+            selected_item = form.cleaned_data['title', 'text']
+            return render(request, 'web3/document_list.html', {'item': selected_item})
+    else:
+        form = DocumentForm()
+    return render(request, 'web3/document_list.html', {'form': form})
